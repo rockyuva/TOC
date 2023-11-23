21a22b20ad2a0e098eb4c78a70315aadbadb44d4
Yuvaraj <113331128+rockyuva@users.noreply.github.com>
2023-11-23 10:29:23 +0530

    Add files via upload

---
+++ EX;4.C
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int matchS(char *str);
+int matchS(char *str) {
+    if (*str == '0') {
+        str++;
+        if (matchS(str)) {
+            if (*str == '0') {
+                str++;
+                return 1;
+            }
+        }
+    } else if (*str == '1') {
+        str++;
+        if (matchS(str)) {
+            if (*str == '1') {
+                str++;
+                return 1;
+            }
+        }
+    } else if (*str == '\0') {
+        return 1;
+    }
+    return 0;
+}
+int main() {
+    char input[100];
+    printf("Enter a string: ");
+    scanf("%s", input);
+    if (matchS(input)) {
+        printf("String belongs to the language!\n");
+    } else {
+        printf("String does not belong to the language!\n");
+    }
+    return 0;
+}
+
